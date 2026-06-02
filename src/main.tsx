dd13ae3052159f65c4f9d4f67459d4b2056d7cb6
Bogdan Polonsky <bmp.prog@gmail.com>
2026-06-02 12:52:30 +0400

    Polish VTT scene layout

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import './styles/cinematic-vtt.css';
 initSentry();
 
 const root = document.getElementById('root') as HTMLElement;
-render(<div className="app-loading">Загрузка...</div>, root);
 
 void bootServices().finally(() => {
   render(<SuperApp />, root);
